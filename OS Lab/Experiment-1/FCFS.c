e0b9c58f708a0e2412861fc76c6fdcaf4da8ecb3
204G1A0515 <94680005+204G1A0515@users.noreply.github.com>
2022-09-06 12:24:16 +0530

    Create FCFS.c

---
+++ OS Lab/Experiment-1/FCFS.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: L. Arshia Parveen
+   Roll No: 204G1A0515
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
